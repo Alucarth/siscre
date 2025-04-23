fe43cb6c44f4d3c99006f178c26ee2bbc30a38ae
GabojuaN <gonzofranco@gmail.com>
2025-04-23 09:34:19 -0400

    Ajustes al voucher de pago de cuota

--- application/modules/payments/controllers/Payments.php
+++ application/modules/payments/controllers/Payments.php
@@ -270,9 +270,11 @@ class Payments extends Secure_area implements iData_controller {
                 $data['capital'] =  to_currency($object->payment_amount_capital);
                 //$data['capital'] =  to_currency($object->payment_amount_capital - $object->interest); // el payment_amount_capital tiene el interes sumando por lo cual se esta restando el interes revisar donde se guarda el valor o tener encuenta este datos siempre al momento de hacer consultas
                 $data['interest'] = to_currency($object->interest);
-                $data['operating_expenses_amount'] = to_currency($object->operating_expenses_amount);
-                $data['total'] = to_currency($object->payment_amount_capital+$object->interest+$object->operating_expenses_amount+$lpp);
-                $data["literal"] = Util::convertirNumeroLetra(number_format((float)($object->payment_amount_capital+$object->interest+$object->operating_expenses_amount+$lpp), 2, '.', '')," BOLIVIANOS");
+                //$data['operating_expenses_amount'] = to_currency($object->operating_expenses_amount); //monto de ahorro
+                //$data['total'] = to_currency($object->payment_amount_capital+$object->interest+$object->operating_expenses_amount+$lpp);
+                $data['total'] = to_currency($object->payment_amount_capital+$object->interest+$lpp); //actualización por solicitud no se debe mostrar el monto de ahorro
+                //$data["literal"] = Util::convertirNumeroLetra(number_format((float)($object->payment_amount_capital+$object->interest+$object->operating_expenses_amount+$lpp), 2, '.', '')," BOLIVIANOS");
+                $data["literal"] = Util::convertirNumeroLetra(number_format((float)($object->payment_amount_capital+$object->interest+$lpp), 2, '.', '')," BOLIVIANOS");
                 //$data['total'] = to_currency($object->payment_amount_capital+$object->operating_expenses_amount);// ense caso como se asume que esta el interes se le adiciona los gastos operativos
                 //$data["literal"] = Util::convertirNumeroLetra(number_format((float)($object->payment_amount_capital+$object->operating_expenses_amount), 2, '.', '')," Bs"); //mismo caso que la linea de arriba
                 $has_nextpay = true;
